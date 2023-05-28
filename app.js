27295a051af2a703342426a216af08504b91cea0
lkoczyba <lukasz.koczyba@gmail.com>
2023-05-28 19:24:55 +0200

    display quotes and activate buttons

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 //Show new quote
 function newQuote(){
     //pick quote from array
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
-    console.log(quote);
+    //check if author field is blank and replace it with unknown
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }
+    else{
+        authorText.textContent = quote.author;
+    }
+    authorText.textContent = quote.author;
+
+    //check quote length to determine styling
+    if(quote.text.length>120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 //Get Quotes From API
 async function getQuotes(){
@@ -16,5 +37,16 @@ async function getQuotes(){
         //Catch error here
     }
 }
+
+//tweet quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+//event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 //On load
 getQuotes();
