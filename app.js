44c19f0b8f10063c08505d1c256b847433ab99f5
lkoczyba <lukasz.koczyba@gmail.com>
2023-05-28 18:29:45 +0200

    pick quote from array

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let apiQuotes = [];
-
+//Show new quote
+function newQuote(){
+    //pick quote from array
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    console.log(quote);
+}
 //Get Quotes From API
 async function getQuotes(){
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await  response.json();
-        console.log(apiQuotes);
+        newQuote();
     }catch(error){
         //Catch error here
     }
